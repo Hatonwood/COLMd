be8a3c9a74fadf7de537f0e73e69391d97795fde
Hatonwood <Scar11969@hotmail.com>
2018-04-19 21:18:43 +0100

    Ma

--- COLMd/main.cpp
+++ COLMd/main.cpp
@@ -36,10 +36,14 @@ void menu()
 int main(int argc, char* argv[])
 {
 	cout << "COLMd" << endl << endl;
-	std::string asd = argv[0];
-	std::string treatedFolder = asd.erase(asd.rfind('\\'));
+	std::string temp = argv[0];
+	std::string treatedFolder = temp.erase(temp.rfind('\\'));
 	cout << "Folder: [" << treatedFolder << "]" << endl;
 	colmed mColmed(argv[0]);
+	mColmed.Scan();
+
+
+
 	system("pause");
 	//Load_aux(path_Recebido, LDados);
 }
